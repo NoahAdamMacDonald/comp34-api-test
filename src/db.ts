6aa630a38d75874bd0dae8d0111bdda200795a32
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-03-21 01:09:16 -0400

    database file

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import Database from "bun:sqlite";
+
+//manages database
+export const db = new Database("database.sqlite");
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP
+  );
+`).run();
